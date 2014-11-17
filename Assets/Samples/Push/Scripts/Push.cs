f4a25e241b3948a540307d79d3b170e83c9b18ec
axaubet <alberto.xaubet@edenicgames.com>
2014-11-17 12:09:48 +0100

    Initial push

---
+++ Assets/Samples/Push/Scripts/Push.cs
@@ -0,0 +1,101 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Globalization;
+using System.Text;
+using System;
+using LitJson_Gamedonia;
+
+
+public class Push : MonoBehaviour {
+
+	public Texture2D backgroundImg;
+	public GUISkin skin;
+	
+
+	private string errorMsg = "";
+	private string statusMsg = "";
+	private string console = "";
+
+	void Awake() {
+
+	}
+
+	void Start() {
+
+		GamedoniaUsers.Authenticate(OnLogin);
+		printToConsole ("Starting session with Gamedonia...");
+		
+		//Handle push
+		GDPushService pushService = new GDPushService();
+		pushService.RegisterEvent += new RegisterEventHandler(OnNotification);
+		GamedoniaPushNotifications.AddService(pushService);
+
+	}
+
+	void OnGUI () {
+		
+		GUI.skin = skin;
+
+		GUI.DrawTexture(UtilResize.ResizeGUI(new Rect(0,0,320,480)),backgroundImg);
+
+		GUI.enabled = (statusMsg == "");
+
+		//Text area control
+		GUI.Label(UtilResize.ResizeGUI(new Rect(80,10,220,20)),"Console Log:","LabelBold");
+		GUI.Box (UtilResize.ResizeGUI (new Rect (80, 30, 220, 380)), console);
+
+		//Server Push
+		if (GUI.Button (UtilResize.ResizeGUI(new Rect (80,420, 220, 50)), "Generate Push With Server Code")) {
+			generatePushWithServerCode();
+		}
+
+		if (errorMsg != "") {
+			GUI.Box (new Rect ((Screen.width - (UtilResize.resMultiplier() * 260)),(Screen.height - (UtilResize.resMultiplier() * 50)),(UtilResize.resMultiplier() * 260),(UtilResize.resMultiplier() * 50)), errorMsg);
+			if(GUI.Button(new Rect (Screen.width - 20,Screen.height - UtilResize.resMultiplier() * 45,16,16), "x","ButtonSmall")) {
+				errorMsg = "";
+			}
+		}
+
+		GUI.enabled = true;
+		if (statusMsg != "") {
+			GUI.Box (UtilResize.ResizeGUI(new Rect (80, 240 - 40, 220, 40)), statusMsg);
+		}
+	}
+
+
+	void generatePushWithServerCode() {
+		printToConsole("Requesting server to send push...");
+		GamedoniaScripts.Run("sendpush",new Dictionary<string,object>(),
+		    delegate (bool success, object data) {
+				if (success) {
+					printToConsole("Push requested successfully");
+				}else {
+					printToConsole("Failed request for server push");
+				}
+			}         
+		);
+	}
+
+	private void printToConsole(string msg) {
+		console += msg + "\n";
+	}
+
+	void OnLogin (bool success) {
+
+		statusMsg = "";
+		if (success) {
+			printToConsole("Session started successfully. uid: " + GamedoniaUsers.me._id);		
+		}else {
+			errorMsg = Gamedonia.getLastError().ToString();
+			Debug.Log(errorMsg);
+		}
+
+	}
+
+	void OnNotification(IDictionary notification) {
+		printToConsole("Notification Received: " + JsonMapper.ToJson(notification));
+	}
+
+}
